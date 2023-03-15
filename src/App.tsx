f87006a861d642dea3678c1106cbe9ad865f7cff
NatalieHanusova <natalka.hanusova1@seznam.cz>
2023-03-15 13:18:48 +0100

    ahoj

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Example from './Example';
+
 
 function App() {
   return (
     <div className="App">
+      <Example width={1280} height={720} />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
